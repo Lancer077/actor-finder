8252fe508512b6def00500d7174bb7e87a1c8cbe
Mason Myre <masonmyre@gmail.com>
2024-08-17 17:52:31 -0500

    2+ movies now works
    added functionality for more than two movies
    
    still working on the back button (will likely never get fixed)
    still working on getting a separate group for actors found in most of the given movies as a functionality for when the user enters 3+ movies (about a 50/50 chance this gets finished)
    
    also barely avoided publishing the api key lmao

--- app.py
+++ app.py
@@ -11,6 +11,9 @@ app = Flask(__name__)
 
 #takes in movie title and returns url for the cast list of the movie
 def get_movie_url(title):
+    #title.replace("spiderman", "spider man") #spiderman doesn't work without the hyphen or a space
+    #title.replace("Spiderman", "spider man")
+
     api_key = "" #It's only a matter of time before I accidentally leak this lmao
     search_url = f"http://www.omdbapi.com/?t={title}&apikey={api_key}" #search the movie title using the api key
     #the actual IMDB website blocks bot activity for searches but does not block bots from accessing the cast list
@@ -62,45 +65,68 @@ def get_cast_union(cast1, cast2):
         if(name != ""):
             cast3.append(name)
 
-
     return cast3
 
 def driver_logic(movies):
-    mURL1 = get_movie_url(movies[0])
-    mURL2 = get_movie_url(movies[1])
+    num_of_movies = len(movies)
 
-    cast1 = get_cast(mURL1)
-    cast2 = get_cast(mURL2)
+    cast_URLs = []
 
-    cast3 = get_cast_union(cast1, cast2)
+    for movie in movies:
+        cast_URLs.append(get_movie_url(movie))
+    
+    cast_lists = []
+    for url in cast_URLs:
+        cast_lists.append(get_cast(url))
 
-    return cast3
+    big_cast_list = dict()
+
+    for cast_list in cast_lists:
+        for cast_member in cast_list:
+            if cast_member in big_cast_list: #if cast member was found and was already in the list
+                big_cast_list[cast_member] = big_cast_list[cast_member] + 1 #increment the number of times that cast member was found
+                
+            else: #if cast member was not already in the list
+                big_cast_list[cast_member] = 1 #add the cast member to the list with one movie to their name
+                # I could make it so that if a cast member was not found in the first movie, they are just disregarded but eventually I want to allow for 3 movies to be entered
+                #   and the website would show people who only show up in 2 of the 3 movies
+    
+    final_cast_list = []
+
+    names = big_cast_list.keys()
+
+    for name in names:
+        if big_cast_list[name] == num_of_movies:
+            final_cast_list.append(name)
 
 
+    return final_cast_list
+    
+   
+    
 
 
 #website flask stuff (aka the part of this project I don't like) ----------------------------------
-def remove_vowels(text):
-    return ''.join(c for c in text if c.lower() not in 'aeiou')
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         # Get all movie titles from the form
         movie_titles = request.form.getlist('movie')
-        # Remove vowels from each movie title
+        #perform the driver logic on the movies
         actor_list = driver_logic(movie_titles)
-        
-        #processed_titles = [remove_vowels(title) for title in movie_titles]
-        # Redirect to the display page with the processed titles
+
+
         return redirect(url_for('actors', actors=','.join(actor_list)))
+
     return render_template('index.html')
 
 @app.route('/actors')
 def actors():
-    # Get the processed titles from the query parameters
+    # actor names
     actors = request.args.get('actors', '')
-    # Split the titles back into a list
+    # Split the actors back into a list
     actor_list = actors.split(',')
     return render_template('actors.html', actor_list=actor_list)
 
